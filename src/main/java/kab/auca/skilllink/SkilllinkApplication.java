cf7afc5d261d90aa36bc224365e10a9fae996144
kab-alain <alainbe012@gmail.com>
2024-12-08 16:38:16 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/kab/auca/skilllink/SkilllinkApplication.java
@@ -0,0 +1,13 @@
+package kab.auca.skilllink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilllinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilllinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kab/auca/skilllink/SkilllinkApplicationTests.java
@@ -0,0 +1,13 @@
+package kab.auca.skilllink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilllinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
