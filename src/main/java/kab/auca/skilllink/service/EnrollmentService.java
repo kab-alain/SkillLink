4bc0fe592e8b983aa229e549a09d813413efdff0
Bruce-the-dev <mutsinzibruce@gmail.com>
2024-12-15 14:59:18 +0200

    updated enrollment

--- src/main/java/kab/auca/skilllink/service/EnrollmentService.java
+++ src/main/java/kab/auca/skilllink/service/EnrollmentService.java
@@ -6,18 +6,40 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kab.auca.skilllink.model.Course;
 import kab.auca.skilllink.model.Enrollment;
+import kab.auca.skilllink.model.User;
+import kab.auca.skilllink.repository.CourseRepository;
 import kab.auca.skilllink.repository.EnrollmentRepository;
+import kab.auca.skilllink.repository.UserRepository;
 
 @Service
 public class EnrollmentService {
 
     @Autowired
     private EnrollmentRepository enrollmentRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CourseRepository courseRepository;
 
     // Enroll a user in a course
     public Enrollment enrollUser(Enrollment enrollment) {
-        enrollment.setEnrollmentDate(LocalDate.now()); // Set current date for enrollment
+        // Fetch full User from DB using the userId given in the enrollment's user
+        // object
+        User fetchedUser = userRepository.findById(enrollment.getUser().getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Fetch full Course from DB using the courseId given in the enrollment's course
+        // object
+        Course fetchedCourse = courseRepository.findById(enrollment.getCourse().getCourseId())
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+
+        // Set the fetched entities on the enrollment
+        enrollment.setUser(fetchedUser);
+        enrollment.setCourse(fetchedCourse);
+        enrollment.setEnrollmentDate(LocalDate.now());
+
         return enrollmentRepository.save(enrollment);
     }
 
